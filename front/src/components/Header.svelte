347d009e040d60330e5daf50c7d3b6dd7a145587
maaaashi <doyaseizin.7@gmail.com>
2023-02-19 22:52:00 +0900

    アイコンの変更

--- front/src/components/Header.svelte
+++ front/src/components/Header.svelte
@@ -3,14 +3,14 @@
   import { Datepicker } from 'svelte-calendar';
   import { page } from '$app/stores';
   import { storeSelectDate } from '../store';
+  import rep from '$lib/images/rep.jpeg'
 </script>
 
 <Navbar>
   <NavBrand href="/">
     <img
-      src="https://flowbite.com/docs/images/logo.svg"
+      src={rep}
       class="mr-3 h-6 sm:h-9"
-      alt="Flowbite Logo"
     />
     <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
       SHOPPING-MEMO
